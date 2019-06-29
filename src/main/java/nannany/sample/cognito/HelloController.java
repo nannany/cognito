c66cf547e8d84e33ff1a6c450d4ae70fac69c7e8
nannany <ymym1990ymym@gmail.com>
2019-06-29 21:17:08 +0900

    CORS設定

--- src/main/java/nannany/sample/cognito/HelloController.java
+++ src/main/java/nannany/sample/cognito/HelloController.java
@@ -1,18 +1,22 @@
 package nannany.sample.cognito;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello!";
-    }
+	@CrossOrigin
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello!";
+	}
 
-    @GetMapping("/goodbye")
-    public String goodbye() {
-        return "Goodbye!";
-    }
+	@CrossOrigin
+	@GetMapping("/goodbye")
+	public String goodbye() {
+		return "Goodbye!";
+	}
 }
--- src/main/java/nannany/sample/cognito/SecurityConfig.java
+++ src/main/java/nannany/sample/cognito/SecurityConfig.java
@@ -14,6 +14,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.hasAnyAuthority("SCOPE_nannany.sample.cognito/hello.read", "SCOPE_aws.cognito.signin.user.admin")
 				.antMatchers(HttpMethod.GET, "/goodbye")
 				.hasAnyAuthority("SCOPE_nannany.sample.cognito/goodbye.read", "SCOPE_aws.cognito.signin.user.admin");
+
 		http.oauth2ResourceServer().jwt();
 	}
+
+//	private CorsConfigurationSource corsConfigurationSource() {
+//		CorsConfiguration corsConfiguration = new CorsConfiguration();
+//		corsConfiguration.addAllowedMethod("GET");
+//		corsConfiguration.addAllowedOrigin("http://localhost:3000");
+//
+//		UrlBasedCorsConfigurationSource corsSource = new UrlBasedCorsConfigurationSource();
+//		corsSource.registerCorsConfiguration("/hello", corsConfiguration);
+//		corsSource.registerCorsConfiguration("/goodbye", corsConfiguration);
+//
+//		return corsSource;
+//	}
 }
